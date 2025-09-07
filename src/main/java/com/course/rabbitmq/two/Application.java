55be1c6105700675d5d83b6aca6b8fee6146077b
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-09-07 19:38:28 +0200

    9.Reliable Publish (65)

--- src/main/java/com/course/rabbitmq/two/Application.java
+++ src/main/java/com/course/rabbitmq/two/Application.java
@@ -1,10 +1,13 @@
 package com.course.rabbitmq.two;
 
-import com.course.rabbitmq.two.producer.SingleActiveProducer;
+import com.course.rabbitmq.two.entity.DummyMessage;
+import com.course.rabbitmq.two.producer.ReliableProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -14,10 +17,20 @@ public class Application implements CommandLineRunner {
 	}
 
 	@Autowired
-	private SingleActiveProducer producer;
+	private ReliableProducer producer;
 
     @Override
-    public void run(String... args) {
-        producer.sendDummy();
+    public void run(String... args) throws InterruptedException {
+		var dummyMessage = new DummyMessage("Dummy content", 1);
+
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("Calling sendDummyToInvalidExchange()");
+        producer.sendDummyToInvalidExchange(dummyMessage);
+
+		TimeUnit.SECONDS.sleep(2);
+
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("Calling sendDummyWithInvalidRoutingKey()");
+		producer.sendDummyWithInvalidRoutingKey(dummyMessage);
     }
 }
---
+++ src/main/java/com/course/rabbitmq/two/producer/ReliableProducer.java
@@ -0,0 +1,57 @@
+package com.course.rabbitmq.two.producer;
+
+import com.course.rabbitmq.two.entity.DummyMessage;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ReliableProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReliableProducer.class);
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    private void registerCallback() {
+        rabbitTemplate.setConfirmCallback((correlation, ack, reason) -> {
+            if (correlation == null) {
+                return;
+            }
+            if (ack) {
+                LOG.info("Message with correlation {} is published", correlation.getId());
+            } else {
+                LOG.warn("Invalid exchange. Message with correlation {} is NOT published", correlation.getId());
+            }
+        });
+        rabbitTemplate.setReturnsCallback(returned -> {
+            LOG.info("return callback");
+
+            if (returned.getReplyText() != null && returned.getReplyText().equalsIgnoreCase("NO_ROUTE")) {
+                var id = returned.getMessage().getMessageProperties().getHeader("spring_returned_message_correlation").toString();
+                LOG.warn("Invalid routing key for id: {}", id);
+            }
+        });
+    }
+
+    public void sendDummyWithInvalidRoutingKey(DummyMessage message) {
+        var correlationData = new CorrelationData(UUID.randomUUID().toString());
+
+        rabbitTemplate.convertAndSend("x.dummy2", "invalidRoutingKey", message, correlationData);
+    }
+
+    public void sendDummyToInvalidExchange(DummyMessage message) {
+        var correlationData = new CorrelationData(UUID.randomUUID().toString());
+
+        rabbitTemplate.convertAndSend("invalidExchange", "", message, correlationData);
+    }
+
+
+}
